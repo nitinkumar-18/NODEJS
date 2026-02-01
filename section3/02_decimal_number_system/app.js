a32eda891caac12bacdf6c5da59b5578a4502637
Tanish-Tandon <tandontanish99@gmail.com>
2026-02-01 17:12:54 +0530

    data representing in computing

---
+++ section3/02_decimal_number_system/app.js
@@ -0,0 +1,26 @@
+const digitsList1=[2,4,6,5];
+const digitsList2=[7,3,2];
+
+
+const num1=2*1 + 4*10 + 6*100 + 5*1000;
+const num2=7*Math.pow(10,0) + 3*Math.pow(10,1) + 2*Math.pow(10,2);
+
+
+function digitsToNumber(digits){
+    return digits.reduce((acc,curr,index)=>acc+curr*Math.pow(10,index),0);
+    
+    // ((digit,index)=>{
+    //     num+=digit * Math.pow(10,index);
+
+
+
+   
+    return num;
+}
+
+// console.log(num1);
+
+// phele kudh ka nu,mber system hai
+// console.log(num2);
+
+
