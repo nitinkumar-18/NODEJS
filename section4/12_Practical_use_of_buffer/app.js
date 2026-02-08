c5a890bc5b1e070f4682bebf2363a1c34f600727
Tanish-Tandon <tandontanish99@gmail.com>
2026-02-08 18:47:17 +0530

    LIMITATION OF BUFFER

---
+++ section4/12_Practical_use_of_buffer/app.js
@@ -0,0 +1,18 @@
+
+// yaha humne dekha  ki file read karte hai toh buffer ke sath kaam karte hai
+
+// import fs from "fs/promises";
+
+
+// buffer hum theroritically nhi practically bhi use hota hai
+
+
+
+console.log(a);// humko hex value buffer ke form me milega
+// agar humko string me chahiye to hume usko convert karna padega. humko jo buffer mil rha hai vo nodes js ka buffer hai
+
+
+
+
+// console.log(a.byteLength);
+
---
+++ section4/js/script.js
@@ -0,0 +1,18 @@
+
+// UI SAI ARRAY BUFFER BHI BHEJ SKTE HAI JARURO NHI HAI STRING HI BHEJEDE
+
+
+const a=await fs.readFile("file.txt");
+const uint8Array = new Uint8Array(a);
+
+uint8Array[0]=97;
+uint8Array[1]=98;
+uint8Array[2]=99;
+uint8Array[3]=100;
+
+
+
+fetch("http://localhost:3000/",{
+    method:"POST",
+    body:"TANISH TANDON HERE NOW"
+}).then((res)=>res.text()).then((data)=>console.log(data));
