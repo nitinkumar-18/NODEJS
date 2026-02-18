50b5c329286758330365b7943904ab7022047974
Tanish-Tandon <tandontanish99@gmail.com>
2026-02-19 00:22:15 +0530

    WRITING TO A FILE WITH FD

---
+++ section6/21_WRITING_TO_A_FILE_DESCRIPTOR/app.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+
+const fd=fs.openSync("text.txt","w");
+
+
+// const buff=Buffer.from("123");
+
+// fs.write(fd,'TANISH',(err,bytesWritten,writtenData)=>{
+
+//     console.log(bytesWritten);
+//     console.log(writtenData);
+
+// })
+
+
+
+
+
+
