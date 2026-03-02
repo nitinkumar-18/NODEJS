edbc18b3e4b0174ad617268908f6a75415cadd5e
Tanish-Tandon <tandontanish99@gmail.com>
2026-03-02 13:43:01 +0530

    FILE TRANSFER

---
+++ section 8/11_IMP_RESPONSE/app.js
@@ -0,0 +1,149 @@
+// import { createReadStream } from "node:fs";
+// import { open } from "node:fs/promises";
+// import net from "node:net";
+
+// const server = net.createServer(async (socket) => {
+  // const fileHandle = await open(
+  //   "C:\\Users\\anura\\OneDrive\\Desktop\\4k-video.MP4"
+  // );
+//   const fileHandle = await open("story.mp4");
+//   const { size } = await fileHandle.stat();
+//   const readStream = fileHandle.createReadStream();
+//   socket.write("HTTP/1.1 200 OKAY\n");
+//   socket.write("Content-Type: video/mp4\n");
+  // socket.write("Content-Type: text/txt; charset=utf-8\n");
+//   socket.write(`Content-Length: ${size}\n`);
+//   socket.write("Content-Disposition: attachment; filename=story.mp4");
+//   socket.write("\n\n");
+
+  // socket.end();
+  // const readStream = createReadStream("story.mp4");
+  // const readStream = createReadStream("river.webp");
+  // const readStream = createReadStream("numbers.txt");
+  // const readStream = createReadStream(
+  //   "C:\\Users\\anura\\OneDrive\\Desktop\\4k-video.MP4"
+  // );
+//   readStream.pipe(socket);
+  // socket.end('{"name": "Anurag"}')
+//   readStream.on("end", () => {
+//     console.log("File ended");
+//   });
+
+//   socket.on("data", (chunk) => {
+//     console.log(chunk.toString());
+//   });
+
+//   socket.on("close", () => {
+//     console.log(socket.remoteAddress, ": Client disconnected");
+//   });
+
+//   socket.on("error", () => {
+//     console.log("Client Lost");
+//   });
+//   console.log("Client Connected", socket.remoteAddress);
+// });
+
+// server.listen(4000, "0.0.0.0", () => {
+//   console.log("Server started on port 4000");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import {open} from "node:fs/promises";
+import { createReadStream } from "node:fs";
+import net from "node:net";
+
+const server = net.createServer(async(socket) => {
+  // socket.write("HTTP/1.1 200 OKAY \nAccess-Control-Allow-Origin:*\n\n");
+  // socket.end();
+
+
+  const fileHandle=await open("numbers.txt");
+  
+
+  const {size}=await fileHandle.stat();
+
+
+  const readStream=fileHandle.createReadStream();
+
+// yeh niche sabh header hai jo client ko bheja ja raha hai
+  socket.write("HTTP/1.1 200 OKAY\n");
+    socket.write("Access-Control-Allow-Origin:*\n");
+    socket.write("Content-Type:text/txt; charset=utf-8\n");    socket.write("Access-Control-Expose-Header:Hello,name*\n");
+    socket.write("hello world\n");
+    socket.write(`Content-length: ${size}\n`);// kitne byte ka data bheja ja raha hai ussai jyada data bhejna allowed nahi hai
+
+    socket.write("\n\n");
+
+  
+  // const readStream = createReadStream("river.webp");
+  // const readStream = createReadStream("numbers.txt");
+  
+  readStream.pipe(socket);
+  // socket.write("abcdefgh");
+  // socket.end();
+  readStream.on("end", () => {
+    console.log("File ended");
+  });
+
+
+  socket.on("data", (chunk) => {
+    console.log(chunk.toString());
+  });
+
+  socket.on("close", () => {
+    console.log(socket.remoteAddress, ": Client disconnected");
+  });
+
+  socket.on("error", () => {
+    console.log("Client Lost");
+  });
+  console.log("Client Connected", socket.remoteAddress);
+});
+
+server.listen(4000, "0.0.0.0", () => {
+  console.log("Server started on port 4000");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// jo hi hii hai vo responsse ka  header hai
+
+
+// HTTP/1.1\n\nhii
