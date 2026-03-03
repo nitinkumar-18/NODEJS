e8f4d39e588f4852e36d5d975590e3533e0aabe4
Tanish-Tandon <tandontanish99@gmail.com>
2026-03-03 14:38:09 +0530

    ERROR RESOLVE

--- section 8/20_CREATE_ONLINE_STORAGE/app.js
+++ section 8/20_CREATE_ONLINE_STORAGE/app.js
@@ -1,93 +1,39 @@
-import http from 'http';
-import {open,readdir,readFile} from "fs/promises";
+import { open, readdir, readFile } from "fs/promises";
+import http from "http";
 
-const server=http.createServer(async(req,res)=>{
-
-    if(req.url === "/"){
-
-const itemList=await readdir('./storage');
-
-
-// console.log(itemList);
-
-
-let dynamicHTML="";
-
-itemList.forEach((item)=>{
-  dynamicHTML += `<a href="/${item}">${item}</a><br>`;
-});
-
-
-
-
-console.log(dynamicHTML);
-
-    res.end(`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>My files</h1>
-
-    <div>
-
-    ${dynamicHTML}
-    </div>
-    
-</body>
-
-
-</html>`)
-
-    }
-
-
-    else {
-
-    // favicon ignore
-    if (req.url === "/favicon.ico") {
-        res.statusCode = 204;
-        return res.end();
-    }
-
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/favicon.ico") return res.end("No favicon.");
+  if (req.url === "/") {
+    serveDirectory(req, res);
+  } else {
     try {
-        const filePath = `./storage${decodeURIComponent(req.url)}`;
-        const fileHandle = await open(filePath);
-
-        const stats = await fileHandle.stat();
-
-        // agar directory hai to open mat karo
-        if (stats.isDirectory()) {
-            res.statusCode = 403;
-            return res.end("Cannot open directory");
-        }
-
+      const fileHandle = await open(`./storage${decodeURIComponent(req.url)}`);
+      const stats = await fileHandle.stat();
+      if (stats.isDirectory()) {
+        serveDirectory(req, res);
+      } else {
         const readStream = fileHandle.createReadStream();
-
-        // stream error handle karo (important)
-        readStream.on("error", () => {
-            res.statusCode = 500;
-            res.end("Stream Error");
-        });
-
         readStream.pipe(res);
-
+      }
     } catch (err) {
-        res.statusCode = 404;
-        res.end("NOT FOUND");
+      console.log(err.message);
+      res.end("Not Found!");
     }
+  }
 });
 
+async function serveDirectory(req, res) {
+  const itemsList = await readdir(`./storage${req.url}`);
+  let dynamicHTML = "";
+  itemsList.forEach((item) => {
+    dynamicHTML += `<a href=".${
+      req.url === "/" ? "" : req.url
+    }/${item}">${item}</a><br>`;
+  });
+  const htmlBoilerplate = await readFile("./boilerplate.html", "utf-8");
+  res.end(htmlBoilerplate.replace("${dynamicHTML}", dynamicHTML));
+}
 
-
-
-
-
-
-
-server.listen(2200,"0.0.0.0",()=>{
-    console.log("SERVER STARTED");
+server.listen(2200, "0.0.0.0", () => {
+  console.log("Server started");
 });
