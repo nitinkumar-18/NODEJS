eb1b465411f8fa96ad8f7075cbebb313e43841e0
Tanish-Tandon <tandontanish99@gmail.com>
2026-03-05 14:16:00 +0530

    BUG IN FORWARDING THE VIDEO FILE

---
+++ section 9/02_SERVE_STATIC_FILES/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import {open} from "fs/promises";
+
+
+const app = express();
+const port = 2700;
+
+
+/// yeh ek middleware hai
+// behind the scene yhe bhi stream use kar rha hai
+app.use(express.static("public"));
+
+
+
+
+app.get("/", (req, res) => {
+  res.end("Hello World!!");
+});
+
+
+
+
+// isme humko isko cursor sai forward nhi kar skte hai yeh dikaat hai aur code bhi jyada line ka hai yeh
+app.get("/test",async(req,res)=>{
+  const fileHandle=await open("STARS.mp4");
+  const readStream=fileHandle.createReadStream();
+
+  const stats=await fileHandle.stat();
+  res.setHeader("Content-Length",stats.size);
+  res.setHeader("Content-Type","video/mp4");
+  readStream.pipe(res);
+
+})
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
