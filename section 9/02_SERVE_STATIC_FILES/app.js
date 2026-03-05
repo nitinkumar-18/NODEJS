fcae992559d032b9092ecb3141f26853bfb108cb
Tanish-Tandon <tandontanish99@gmail.com>
2026-03-05 14:21:49 +0530

    BUG RESOLVED BY USING A sendFile method

--- section 9/02_SERVE_STATIC_FILES/app.js
+++ section 9/02_SERVE_STATIC_FILES/app.js
@@ -22,14 +22,22 @@ app.get("/", (req, res) => {
 
 // isme humko isko cursor sai forward nhi kar skte hai yeh dikaat hai aur code bhi jyada line ka hai yeh
 app.get("/test",async(req,res)=>{
-  const fileHandle=await open("STARS.mp4");
-  const readStream=fileHandle.createReadStream();
 
-  const stats=await fileHandle.stat();
-  res.setHeader("Content-Length",stats.size);
-  res.setHeader("Content-Type","video/mp4");
-  readStream.pipe(res);
 
+  // const fileHandle=await open("STARS.mp4");
+  // const readStream=fileHandle.createReadStream();
+
+  // const stats=await fileHandle.stat();
+  // res.setHeader("Content-Length",stats.size);
+  // res.setHeader("Content-Type","video/mp4");
+  // readStream.pipe(res);
+
+
+
+  // THIS sendFile method want absolute path thats why we use this give us the absolute path of any file
+
+
+  res.sendFile(`${import.meta.dirname}/STARS.mp4`);
 })
 
 
