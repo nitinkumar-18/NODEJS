ab1e0301681c6d42591449643bba1b48957301fa
Tanish-Tandon <tandontanish99@gmail.com>
2026-03-06 14:38:17 +0530

    PREFLIGHT REQUEST

--- section 9/05_CORS/app.js
+++ section 9/05_CORS/app.js
@@ -53,11 +53,23 @@ const app=express();
 
 const PORT=2400;
 
-app.use(cors());
 
-app.use(express.static("public"));
+
+
 
 // app.use((req,res,next)=>{
+//   console.log(req.method);
+//   console.log(req.headers);
+//   next();
+// })
+// app.use(cors({
+//   allowedHeaders:"Content-Type",
+//   methods:"PUT",
+//   origin:"*"
+// }));
+
+
+app.use((req,res, next)=>{
   
   // const allowedOrigin=["http://127.0.0.1:5501","http://localhost:5501","http://127.0.0.1:5500"]
 
@@ -66,14 +78,26 @@ app.use(express.static("public"));
   // }
 
   //OR
-//   res.set("Access-Control-Allow-Origin","*");
-//   next(); 
-// })
+  // res.set("Access-Control-Allow-Origin","*");
+//     res.set("Access-Control-Allow-Methods","*");
+//       res.set("Access-Control-Allow-Headers","Content-Type");
+  next(); 
+})
 
+app.use(express.static("public"))
 
 app.get("/api",(req,res)=>{
 
   res.json({message : "GET THE SIDE TANISH HERE"});
+
+});
+
+
+
+
+app.head("/api",(req,res)=>{
+
+  res.json({message : "HEAD  THE SIDE TANISH HERE"});
 
 });
 
@@ -93,6 +117,13 @@ app.put("/api",(req,res)=>{
 
 })
 
+
+
+
+app.delete ("/api",(req,res)=>{
+  res.json({message :"DELETE THIS SIDE TANISH" });
+
+})
 
 
 app.listen(PORT,()=>{
@@ -135,3 +166,28 @@ app.listen(PORT,()=>{
 
 
 // use of cors enable all the preflight features
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// preflight cors mdn
+// some request not trigger preflight those called simple request get head post in simple category
+// form cors follow nhi karta thaaa get post head not more dangerous    put patch delete is some dangerous
+
+
+// cors ke andar get post head allow hote hai. aur kuch hi headers allowed hai iske simple mai bhi 
--- section 9/05_CORS/public/script.js
+++ section 9/05_CORS/public/script.js
@@ -1,3 +1,8 @@
-const response = await fetch("http://localhost:2400/api");
-const data = await response.json();
-console.log(data);
+// const response = await fetch("http://localhost:2400/api");
+// const data = await response.json();
+// console.log(data);
+
+
+
+
+console.log("server");
