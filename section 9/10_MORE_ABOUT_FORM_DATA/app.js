f520364eace65b05ec712aa71f26790235eb18c4
Tanish-Tandon <tandontanish99@gmail.com>
2026-03-12 21:58:40 +0530

    EXTENDED TRUE VS EXTENDED FALSE

--- section 9/10_MORE_ABOUT_FORM_DATA/app.js
+++ section 9/10_MORE_ABOUT_FORM_DATA/app.js
@@ -6,22 +6,22 @@
 import express, { urlencoded } from "express"
 const app=express();
 
-
-
+//extended false means ek dum halka kardia ek dum halka lekin proper nhi deta like parse proper nhi karta 
+// true mai kardet hai nested data ke liye extended true karo plain object ke liye extended false.  but use express.json always 
 app.use(express.static("public"));
-app.use(express.text());
-app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+// app.use(express.text());
+// app.use(express.json());
+// app.use(express.urlencoded({extended:true}))
 
 //decodeURIComponent 
 
 app.post("/user",(req,res)=>{
-  console.log(req.headers['content-type']);
-  req.on("data",(chunk)=>{ 
-    console.log("chunk")
-    console.log(chunk.toString());
+  // console.log(req.headers['content-type']);
+  // req.on("data",(chunk)=>{ 
+  //   console.log("chunk")
+  //   console.log(chunk.toString());
 
-  })
+  // })
   console.log({body :req.body});
 
   res.json({message : "GOT DATA"});
