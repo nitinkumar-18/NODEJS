c607e1d3d19dbde6af8660ce2404fd0204932d8d
Tanish-Tandon <tandontanish99@gmail.com>
2026-03-20 03:10:55 +0530

    server client database collection methods in mongodb

---
+++ section 10/05_MONGO_IN_NODEJS/app.js
@@ -0,0 +1,48 @@
+// import { MongoClient } from "mongodb";
+
+// const client = new MongoClient("mongodb://127.0.0.1:27017/")
+
+// await client.connect()
+
+// const admin = client.db().admin()
+// const allDbs = await admin.listDatabases()
+
+// console.log(allDbs);
+
+// client.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { MongoClient } from "mongodb";
+
+
+
+
+
+const client=new MongoClient("mongodb://localhost:27017/")
+
+await client.connect();// server sai tcp connection banalega
+
+
+
+
+const admin=client.db().admin();
+
+const allDbs=await admin.listDatabases()// saare database return karega jo mongodb server ke upar
+
+
+console.log(allDbs);
+
+client.close();
