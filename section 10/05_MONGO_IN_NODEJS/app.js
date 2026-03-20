183cbee6166e4759cf70595059051d8c63b7ace3
Tanish-Tandon <tandontanish99@gmail.com>
2026-03-20 05:43:01 +0530

    SOME NEW TOPIC I FOUND CURSOR

--- section 10/05_MONGO_IN_NODEJS/app.js
+++ section 10/05_MONGO_IN_NODEJS/app.js
@@ -75,6 +75,103 @@ const client=new MongoClient("mongodb://localhost:27017/");
 await client.connect()
 
 
+const db=client.db("todoApp");
+
+// const collection=db.collection("todos");
+
+
+const collection=db.collection("todos");
+
+// const data=[];
+
+// for(let i=11;i<=25;i++){
+//     data.push({title:`Task ${i}`,completed:i%2==0 ?true:false})
+
+// }
+
+
+const cursor=collection.find();
+
+
+
+// let cnt=0;
+// while(await cursor.hasNext()){
+//     cnt++;
+//     console.log(await cursor.next());
+//     if(cnt===5)break;
+// }
+
+
+
+// console.log(await cursor.hasNext());
+
+// cursor mai ek barr data fetch kar skte hai
+
+// const data=await cursor.toArray();
+// console.log(data);
+
+
+
+// const data2=await cursor.toArray();
+// console.log(data2);
+
+
+
+// console.log(await cursor.hasNext());
+// console.log(await cursor.next());
+
+
+
+
+
+
+// console.log(await cursor.hasNext());
+// console.log(await cursor.next());
+
+
+
+// console.log(await cursor.hasNext());
+
+
+// cursor is an object in the eyes of  js . iske upar specific method karo tabh db call karta hai
+// for(;await cursor.hasNext();){
+    
+// console.log(await cursor.next());
+
+// }
+
+
+
+
+// console.log(await cursor.count());
+
+
+// console.log(await collection.countDocuments());
+
+
+
+
+
+// for await(const document of cursor){
+//     console.log(document);
+// }
+
+// console.log(await cursor.hasNext());
+
+// console.log(await cursor.next());
+// console.log(await cursor.next());
+// console.log(await cursor.next());
+
+// console.log(cursor[Symbol.asyncIterator]);
+
+
+//async iterator ke sath for await use karo
+
+// await collection.insertMany(data);
+
+
+client.close();
+
 
 
 
@@ -92,5 +189,8 @@ client.close();
 
 
 
+// > a.constructor
+// [class Cursor extends AggregateOrFindCursor]
+// 
 
 
