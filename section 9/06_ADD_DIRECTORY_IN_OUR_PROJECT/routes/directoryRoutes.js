253f2366f771250d061b9e0970bea35f9eae2910
Tanish-Tandon <tandontanish99@gmail.com>
2026-03-29 00:37:01 +0530

    AGAIN BACK AND FIX MY DELETE FUNCTIONALITY BUT STILL STUCK IN ONE ERROR NOT UPDATE IN MONGODB

--- section 9/06_ADD_DIRECTORY_IN_OUR_PROJECT/routes/directoryRoutes.js
+++ section 9/06_ADD_DIRECTORY_IN_OUR_PROJECT/routes/directoryRoutes.js
@@ -14,7 +14,9 @@ import { readFile, writeFile, rm } from "fs/promises";
 import crypto from "crypto";
 import validateIdMiddleware from "../middlewares/validateIdMiddleware.js";
 import { Db, ObjectId } from "mongodb";
-import { dir } from "console";
+import { dir, error } from "console";
+import { get } from "https";
+import { extension } from "mime-types";
 
 const router = express.Router();
 
@@ -32,7 +34,10 @@ router.param("id", validateIdMiddleware);
 // =========================
 router.get("/:id?", async (req, res) => {
   // const db=req.db;
+  // const db=req.db;
+
   const db=req.db;
+
 
   const dirCollection=db.collection("directories");
 
@@ -119,12 +124,15 @@ router.post("/:parentDirId?", async (req, res, next) => {
   const dirCollection=db.collection("directories");
 
 
+
   try {
     const directoriesData = JSON.parse(
       await readFile("./directoriesDB.json", "utf-8")
     );
 
     const parentDirId = req.params.parentDirId || user.rootDirId;
+
+    
     const dirname = req.headers.dirname || "NEW FOLDER";
 
     // const parentDir = directoriesData.find(
@@ -255,86 +263,237 @@ router.patch("/:id", async (req, res, next) => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // =========================
 // DELETE DIRECTORY
 // =========================
 router.delete("/:id", async (req, res, next) => {
+  const user=req.user;
+   
   const { id } = req.params;
 
-  try {
-    const directoriesData = JSON.parse(
-      await readFile("./directoriesDB.json", "utf-8")
+
+  const db=req.db;
+
+
+
+  const filesCollection=db.collection('files');
+
+  const dirCollection=db.collection('directories');
+
+  
+
+
+  const dirObjId=new ObjectId(id);
+
+
+ const directoryData= await dirCollection.findOne({_id:dirObjId,userId:req.user._id},
+  {projection : {_id :1}}
+ );
+
+
+ if(!directoryData){
+  return res.status(404).json({error : "DIRECTORY NOT FOUND"});
+ }
+
+
+
+
+
+
+
+  // const files=await filesCollection.find({parentDirId:parentDirObjId}, {projection : {name:1,_id:0}}).toArray();
+
+
+  // const directories=await dirCollection.find({parentDirId:parentDirObjId},{projection:{name:1,_id:0}}).toArray();
+
+
+
+
+  // console.log({files,directories});
+
+
+
+
+
+
+
+
+
+  async function getDirectoryContents(id){
+
+
+  let files=await filesCollection.find({parentDirId:id , userId:user._id}, {projection : {_id:1,extension:1}}).toArray();
+
+
+
+  let directories=await dirCollection.find({parentDirId:id ,   userId: user._id},{projection:{_id:1,extension:1}}).toArray();
+
+
+
+
+  // for(const {_id,name} of files){
+
+  //   console.log(name);
+  //   // getDirectoryContents(_id);
+
+  //   // await getDirectoryContents(
+  //   //   _id
+  //   // );
+    
+
+
+  // }
+
+
+  for(const {_id,name} of directories){
+
+    console.log(name);
+    // getDirectoryContents(_id);
+
+   const{files :childFiles,directories:childDirectories}= await getDirectoryContents(
+      _id
+      
     );
 
-    const filesData = JSON.parse(
-      await readFile("./filesDB.json", "utf-8")
-    );
 
-    const dirIndex = directoriesData.findIndex(
-      (dir) => dir.id === id
-    );
 
-    if (dirIndex === -1) {
-      return res.status(404).json({ message: "Directory not found" });
-    }
+    files=[...files,...childFiles];
 
-    const directoryData = directoriesData[dirIndex];
+    
 
-    // ❌ remove directory
-    directoriesData.splice(dirIndex, 1);
+directories=[...directories,...childDirectories];
 
-    // 🧹 delete files inside
-    for (const fileId of directoryData.files) {
-      const fileIndex = filesData.findIndex((file) => file.id === fileId);
-
-      if (fileIndex !== -1) {
-        const fileData = filesData[fileIndex];
-
-        await rm(`./storage/${fileId}${fileData.extension}`);
-        filesData.splice(fileIndex, 1);
-      }
-    }
-
-    // 🧹 delete child directories (simple version)
-    for (const childId of directoryData.directories) {
-      const childIndex = directoriesData.findIndex(
-        (d) => d.id === childId
-      );
-
-      if (childIndex !== -1) {
-        directoriesData.splice(childIndex, 1);
-      }
-    }
-
-    // 🧹 remove from parent
-    const parentDir = directoriesData.find(
-      (dir) => dir.id === directoryData.parentDirId
-    );
-
-    if (parentDir) {
-      parentDir.directories = parentDir.directories.filter(
-        (dirId) => dirId !== id
-      );
-    }
-
-    await writeFile(
-      "./filesDB.json",
-      JSON.stringify(filesData, null, 2)
-    );
-
-    await writeFile(
-      "./directoriesDB.json",
-      JSON.stringify(directoriesData, null, 2)
-    );
-
-    return res.status(200).json({ message: "Directory Deleted!" });
-
-  } catch (err) {
-    console.log("DELETE ERROR:", err);
-    next(err);
   }
+
+
+
+
+
+
+
+
+
+  // for(const {_id,name} of files){
+
+  //   console.log(name);
+  //   // getDirectoryContents(_id);
+
+  //   await getDirectoryContents(
+  //     _id
+  //   );
+    
+
+
+  // }
+
+
+
+
+  return {files,directories};
+
+
+
+
+  }
+
+
+  // const data=await getDirectoryContents(dirObjId);
+  // console.log(data);
+
+
+  
+
+
+
+
+  const {files,directories}=await getDirectoryContents(dirObjId);
+
+
+
+// 🔍 DEBUG LOGS (YAHI LAGANA HAI)
+console.log("FILES IDS:", files.map(f => f._id));
+console.log("DIR IDS:", directories.map(d => d._id));
+
+
+  console.log(files,directories);
+
+  for(const {_id,extension} of files){
+
+    await rm(`./storage/${_id.toString()}${extension}`);
+ 
+  }
+
+
+
+  // console.log(files);
+  // console.log(files.map(({_id})=>_id));
+  // console.log(directories);
+  // console.log(directories.map(({_id})=>_id));
+
+
+
+
+
+ await  filesCollection.deleteMany({_id : {$in :files.map(({_id})=>_id)}})
+
+
+
+ await  dirCollection.deleteMany({_id : {$in :[...directories.map(({_id})=>_id),dirObjId]}})
+
+
+
+
+  // return res.json({message : " FILES DELETED SUCCESSFULLY"})
+
+
+  return res.json({message : "FILES DELETED SUCCESSFULLY"})
+  
+
+
 });
 
 
 
 export default router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
