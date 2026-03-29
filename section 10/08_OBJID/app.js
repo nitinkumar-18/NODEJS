cb0328cd511f5278b2e632e7c0c7d53bb03dfead
Tanish-Tandon <tandontanish99@gmail.com>
2026-03-29 19:04:50 +0530

    PLAY WITH COMMANDS

--- section 10/08_OBJID/app.js
+++ section 10/08_OBJID/app.js
@@ -178,25 +178,227 @@
 
 
 
+
+
+// import { MongoClient } from "mongodb";
+
+
+// const client=new MongoClient("mongodb://localhost:27017");
+
+
+// await client.connect();
+
+// const db=client.db();
+
+// const collection=db.collection("users");
+
+// const result=await collection.updateOne(
+//     {name :"Ai"},
+//     {$set:{age:90}},
+//     {upsert : true}
+// );
+
+
+// console.log(result);
+
+// client.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// db.runCommand() in mongosh shell
+//db.Command({ listDatabases: 1 }); work normally
+
+// kuch command ke liye admin database ki permission chaiyeh hoti hai jo essse hi kahi sai bhi run hoghi
+
+
+
+
+
+
+
+
+//  errmsg: 'listDatabases may only be run against the admin database.',
+//const result=await db.command({listDatabases :1 });
+
+
+
+// method sai bhi db.listCollections() thik issai bhi ho skta hai thik aur suno  method yeh command ka hi abstraction hai but command bhaut powerful hote hai
+
+
+
+
 import { MongoClient } from "mongodb";
 
 
-const client=new MongoClient("mongodb://localhost:27017");
+const client=new MongoClient("mongodb://localhost:27017/newDB");
 
 
 await client.connect();
 
 const db=client.db();
 
-const collection=db.collection("users");
 
-const result=await collection.updateOne(
-    {name :"Ai"},
-    {$set:{age:90}},
-    {upsert : true}
-);
+// const result=await db.command({listCommands :1 });
 
 
-console.log(result);
+
+
+
+// const result=await db.command({listDatabases :1 });
+
+
+
+// const result=await db.command({listCollections :1 });
+
+// db.listCollections();
+
+
+// console.log(result.cursor.firstBatch);
+
+
+
+//OR 
+
+// console.log(await db.listCollections().toArray());
+
+
+
+
+
+
+
+
+
+
+// const collection=db.collection("fruits");
+// await collection.insertOne({name : "MANGO"})
+
+
+
+
+
+
+
+
+
+
+//yeh essa collection create karega jo completely khali hogha
+
+
+//via method we create collection
+// await db.createCollection("vegetables");
+//OR
+
+// const result=await db.command({create : "GRAINS"});
+
+// console.log(result);
+
+
+
+
+
+// empty database nhi bana skte hai essa koi command nhi hai
+
+
+
+
+
+
+
+// const result=await db.command({drop : "GRAINS"});
+
+// console.log(result);
+
+
+
+
+
+// const result=await db.collection("vegetables").rename("veggies");
+
+// console.log(result);
+
+
+
+///db.runCommand({hostInfo:1})
+
+
+// test> db.runCommand({hostInfo:1})
+// {
+//   system: {
+//     currentTime: ISODate('2026-03-29T13:20:27.826Z'),
+//     hostname: 'Tanishs-MacBook-Air.local',
+//     cpuAddrSize: 64,
+//     memSizeMB: Long('16384'),
+//     memLimitMB: Long('16384'),
+//     numCores: 10,
+//     numCoresAvailableToProcess: 10,
+//     numPhysicalCores: 10,
+//     numCpuSockets: 1,
+//     cpuArch: 'arm64',
+//     numaEnabled: false,
+//     numNumaNodes: 1
+//   },
+//   os: { type: 'Darwin', name: 'Mac OS X', version: '25.3.0' },
+//   extra: {
+//     versionString: 'Darwin Kernel Version 25.3.0: Wed Jan 28 20:49:24 PST 2026; root:xnu-12377.81.4~5/RELEASE_ARM64_T8132',
+//     alwaysFullSync: 0,
+//     nfsAsync: 0,
+//     model: 'Mac16,12',
+//     cpuString: 'Apple M4',
+//     pageSize: 16384,
+//     scheduler: 'edge'
+//   },
+//   ok: 1
+// }
+// test> 
+
+
+
+
+await db.command({ dropDatabase: 1 });
+
+
+
+
+
+
 
 client.close();
